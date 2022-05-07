bcc56a40addf193532b2a0848a7ca39e26c30634
Darryl <51407026+ddaarrrryyll@users.noreply.github.com>
2022-05-07 17:05:34 +0800

    Update misc_component.py

--- components/misc_component.py
+++ components/misc_component.py
@@ -18,7 +18,7 @@ async def joinroom(ctx: lightbulb.Context):
     states = ctx.bot.cache.get_voice_states_view_for_guild(ctx.guild_id)
     voice_state = [state async for state in states.iterator().filter(lambda i: i.user_id == caller)]
     if not voice_state:
-        return await ctx.respond("Connect to a voice channel first.", hikari.MessageFlag.EPHEMERAL)
+        return await ctx.respond("Connect to a voice channel first.", flag=hikari.MessageFlag.EPHEMERAL)
 
     voice_channel_id = voice_state[0].channel_id
 
